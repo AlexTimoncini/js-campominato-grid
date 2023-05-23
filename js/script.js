b735b8f97e16e9e4de0438d2a41d6807cc234785
AlexTimoncini <alextimonci@gmail.com>
2023-05-23 19:04:58 +0200

    added select to chose grid size html only

--- js/script.js
+++ js/script.js
@@ -4,6 +4,12 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe. 
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
+// Bonus
+// Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+// con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+// con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+// con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+
 let gridParent = document.getElementById('grid');
 let gridGeneratorDom = document.getElementById('gridGenerator');
 
@@ -25,6 +31,7 @@ function gridGenerator(cellNumber, gridContainer){
         gridContainer.append(cell);
         cell.addEventListener('click', function(){
             cell.classList.toggle('active');
+            console.log('you clicked on ' + i);
         });
     }
 };
